68b16b3574a44eaa1c39a76719d2ca11fb0f875a
NadinAlJariry <nadeen_hassan@live.com>
2019-04-28 11:56:15 +0200

    Collapsible content added

--- js/resume.js
+++ js/resume.js
@@ -26,3 +26,19 @@
   });
 
 })(jQuery); // End of use strict
+
+(function() {
+    const headings = document.querySelectorAll('h3')
+
+    Array.prototype.forEach.call(headings, heading => {
+      let btn = heading.querySelector('button')
+      let target = heading.nextElementSibling
+
+      btn.onclick = () => {
+        let expanded = btn.getAttribute('aria-expanded') === 'true' || false
+
+        btn.setAttribute('aria-expanded', !expanded)
+        target.hidden = expanded
+      }
+    })
+  })()
